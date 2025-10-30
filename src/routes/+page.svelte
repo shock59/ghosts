47ab9c82d6903733621cfeabdcb9bc0d7f8ced51
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-30 16:37:15 +1100

    Log mouse movements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,19 @@
   import { onMount } from "svelte";
 
   let mouseCoordinates: [number, number] = $state([0, 0]);
+  let session: [number, number][] = $state([]);
 
   function mouseMoved(event: MouseEvent) {
     mouseCoordinates = [event.x, event.y];
   }
 
+  function logInterval() {
+    session.push(mouseCoordinates);
+  }
+
   onMount(() => {
     document.addEventListener("mousemove", mouseMoved);
+    setInterval(logInterval, 50);
   });
 </script>
 
