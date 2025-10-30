10d5bb5c5c46b8071b9e7ef536832fd8da1cf62c
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-30 20:46:44 +1100

    Remove some unneeded imports

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import { createServer } from "node:http";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 
 const app = express();
