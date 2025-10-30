dad0580c600ce24379ab81bb943f9f161d839896
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-30 20:45:15 +1100

    Set up socket.io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("server");
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => console.log(`Disconnected: ${socket.id}`));
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
