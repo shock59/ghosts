51d6a4c7123f07bf4c7f872e1504791d3f547266
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-30 16:40:57 +1100

    Replay mouse movements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,36 @@
 
   let mouseCoordinates: [number, number] = $state([0, 0]);
   let session: [number, number][] = $state([]);
+  let interval: number | undefined = $state();
+  let mode: "recording" | "replay" = $state("recording");
+  let replayIndex = $state(0);
 
   function mouseMoved(event: MouseEvent) {
     mouseCoordinates = [event.x, event.y];
   }
 
   function logInterval() {
-    session.push(mouseCoordinates);
+    if (mode == "recording") {
+      session.push(mouseCoordinates);
+    } else {
+      mouseCoordinates = session[replayIndex];
+      replayIndex++;
+    }
+  }
+
+  function stopRecording() {
+    mode = "replay";
+    document.removeEventListener("mousemove", mouseMoved);
   }
 
   onMount(() => {
     document.addEventListener("mousemove", mouseMoved);
-    setInterval(logInterval, 50);
+    interval = setInterval(logInterval, 50);
   });
 </script>
 
 <p>{mouseCoordinates}</p>
+<button onclick={stopRecording}>Stop recording</button>
 
 <div class="cursor" style="left: {mouseCoordinates[0]}px; top: {mouseCoordinates[1]}px;"></div>
 
@@ -28,5 +42,6 @@
     width: 20px;
     height: 20px;
     background: red;
+    pointer-events: none;
   }
 </style>
