782e572bdd09669393a619e3e9c3b4a46bb72814
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-30 16:29:50 +1100

    Track mouse

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-Ghosts
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let mouseCoordinates: [number, number] = $state([0, 0]);
+
+  function mouseMoved(event: MouseEvent) {
+    mouseCoordinates = [event.x, event.y];
+  }
+
+  onMount(() => {
+    document.addEventListener("mousemove", mouseMoved);
+  });
+</script>
+
+<p>{mouseCoordinates}</p>
