1dde6a025194eaa85c9ad20fdcfda7ce51d349de
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-31 14:57:47 +1100

    Random ghost colours

--- frontend/src/components/GhostReplay.svelte
+++ frontend/src/components/GhostReplay.svelte
@@ -3,6 +3,8 @@
 
   let { session }: { session: Coordinates[] } = $props();
 
+  const colors = ["red", "orange", "green", "blue", "purple", "magenta"];
+
   let element: HTMLElement;
 
   let sessionIndex: number = $state(0);
@@ -57,7 +59,7 @@
 >
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
     ><path
-      fill="purple"
+      fill={colors[Math.floor(Math.random() * colors.length)]}
       d="M12 1a9.01 9.01 0 0 0-9 9v12a1 1 0 0 0 1.8.6l2.029-2.7 1.276 2.552a1 1 0 0 0 1.7.153L12 19.667l2.2 2.933a1 1 0 0 0 1.7-.153l1.271-2.547 2.029 2.7a1 1 0 0 0 .8.4 1 1 0 0 0 1-1V10a9.01 9.01 0 0 0-9-9zm7 18-1.2-1.6a.991.991 0 0 0-.89-.4 1 1 0 0 0-.8.549l-1.276 2.552L12.8 17.4a1 1 0 0 0-1.6 0l-2.029 2.705L7.9 17.553A1 1 0 0 0 7.09 17a.989.989 0 0 0-.89.4L5 19v-9a7 7 0 0 1 14 0zm-9-9v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 2 0zm6 0v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 2 0z"
     /></svg
   >
