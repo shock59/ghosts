e394240b2a25ff554d156555700b5b1601cad8b2
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-01 22:52:37 +1100

    Update session storage to not use socket IDs This has the benefits of not overwriting old ghost data if someone joins with the same ID as someone previous, makes it so that incomplete sessions are not sent to new clients, and makes the code cleaner

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,9 @@ import { JSONFilePreset } from "lowdb/node";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-const db = await JSONFilePreset<{ sessions: Record<string, Session> }>(
-  "db.json",
-  { sessions: {} }
-);
+const db = await JSONFilePreset<{ sessions: Session[] }>("db.json", {
+  sessions: [],
+});
 
 const app = express();
 const server = createServer(app);
@@ -14,27 +13,25 @@ const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
 
 setInterval(() => {
   console.log(
-    `Writing sessions to DB (${
-      Object.values(db.data.sessions).length
-    } sessions stored)`
+    `Writing sessions to DB (${db.data.sessions.length} sessions stored)`
   );
   db.write();
 }, 60_000);
 
 io.on("connection", (socket) => {
   console.log(`Client connected: ${socket.id}`);
-  socket.emit("sessions", Object.values(db.data.sessions));
+  socket.emit("sessions", db.data.sessions);
+
+  let session: Session = [];
 
   socket.on("data", (data: Session) => {
-    db.data.sessions[socket.id] = [
-      ...(db.data.sessions[socket.id] ?? []),
-      ...data,
-    ];
+    session = [...session, ...data];
   });
 
-  socket.on("disconnect", () =>
-    console.log(`Client disconnected: ${socket.id}`)
-  );
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+    db.data.sessions.push(session);
+  });
 });
 
 server.listen(3000, () => {
